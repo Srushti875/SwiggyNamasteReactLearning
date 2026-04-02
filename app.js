ea74eb6f422a1092a4286ce2b6ec5c3184176d50
srushti <patekarsrushti92@gmail.com>
2026-04-02 15:37:49 +0530

    functional component and component composition

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React.createElement ----object-----htmlelement(render)
 
-const Parent =
-React.createElement ( "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [
-      React.createElement("h1", { key: "h1-1" }, "I am an h1 tag"),
-      React.createElement("h1", { key: "h1-2" }, "I am another h1 tag")
-    ]
-  )
-);
+//const heading=React.createElement("h1",{id:"heading"},"React Learning part 2");
+//console.log(heading);
 
-/**
- * 
- 
-const firstElement = React.createElement("h1",{id:"heading"},"I am learning React");*/
-const container = ReactDOM.createRoot(document.getElementById("container"));
-container.render(Parent);
+//jsx is transpiled by parcel(parcel is manager and done by babel)--before it reaches js engine
+//Babel is installed by parcel--it converts code to code react will understand
+//jsx code is transpiled to react.createelement ---js object----html element(render)
 
 
-/** 
-*<div id="parent">
-*   <div id="child">
-*        <h1></h1>
-   </div>
-*</div>
-*/
+//React element
+const JsxHeading = () => (<h1 className="head" tabIndex="1">
+                React Learning using jsx
+                </h1>);
+console.log(JsxHeading);
+
+
+//React component.
+//everything is component in react header,footer,etc.
+//class based components----old way to write code
+
+//functional components------new way to write code
+//const HeadingComponent1 = () =>  {
+//return <h1 className="heading">Functional component</h1>};
+//THIS IS COMPONENT COMPOSITION
+const HeadingComponent2 = () =>  (
+  <div id="container">
+    <JsxHeading/>
+   <h1 className="heading">Functional component</h1>
+   </div>);
+
+//Both are same--if 1 line is 1 only than no need of return
+//const fn1 = () => true;
+
+//const fn2 = () => {
+//  return true;
+//}
+const number=9800
+//tutorial personal practice
+const Prac1 = () => (<h2>This is component which will go inside Prac2</h2>)
+const Prac2 = () =>(
+  <div id="NewContainer">
+    <HeadingComponent2/>
+    <h2>{number}</h2>
+    <Prac1/>
+    <h2>This is component inside which Prac1 will come</h2>
+  </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(jsxHeading);
+root.render(<HeadingComponent2/>);
+root.render(<Prac2/>);
+
+
